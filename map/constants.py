b4e60710ac64d5f2563ccac0d02dd593d845883f
Arce <arcegk@gmail.com>
2015-01-26 15:24:23 -0500

    op

--- map/constants.py
+++ map/constants.py
@@ -1,43 +1,79 @@
+# -*- encoding: utf-8 -*-
+
 CORREGIMIENTOS = (
-	('aa' , 'bb'),
-	('key' , 'value'),
+	('ARROYOHONDO' , 'ARROYOHONDO'),
+	('DAPA' , 'DAPA'),
+	('MULALÓ' , 'MULALÓ'),
+	('EL PEDREGAL' , 'EL PEDREGAL') ,
+	('SAN MARCOS', 'SAN MARCOS'),
+	('SANTA INÉS' , 'SANTA INÉS') ,
+	('YUMBILLO','YUMBILLO'),
+	('LA OLGA' , 'LA OLGA'),
+	('LA BUITRERA' , 'LA BUITRERA'),
+	('MONTAÑITAS' , 'MONTAÑITAS'),
+
 )
 VEREDAS = (
-	('key' , 'value'),
-	('aa' , 'bb'),
+	('ARROYOHONDO' , 'ARROYOHONDO'),
+	('XIXALOA' , 'XIXALOA'),
+	('MIRAVALLE DAPA' , 'MIRAVALLE DAPA'),
+	('ALTO DAPA' , 'ALTO DAPA'),
+	('MEDIO DAPA' , 'MEDIO DAPA'),
+	('PILAS DAPA' , 'PILAS DAPA'),
+	('RINCÓN DAPA' , 'RINCÓN DAPA'),
+	('EL PEDREGAL' , 'EL PEDREGAL'),
+	('FILO Y LAGUNA' , 'FILO Y LAGUNA'),
+	('MULALÓ ', 'MULALÓ '),
+	('PLATANARES' , 'PLATANARES'),
+	('PASO DE LA TORRE' , 'PASO DE LA TORRE'),
+	('EL HIGUERÓN' , 'EL HIGUERÓN' ),
+	('SAN MARCOS' , 'SAN MARCOS'),
+	('MANGA VIEJA' , 'MANGA VIEJA'),
+	('MIRAVALLE NORTE' , 'MIRAVALLE NORTE'),
+	('SANTA INÉS' , 'SANTA INÉS'),
+	('EL CHOCHO' , 'EL CHOCHO'),
+	('TELECOM' , 'TELECOM'),
+	('YUMBILLO', 'YUMBILLO'),
+	('SALAZAR' , 'SALAZAR'),
+	('LA OLGA' , 'LA OLGA'),
+	('LA BUITRERA' , 'LA BUITRERA'),
+	('MONTAÑITAS ' , 'MONTAÑITAS '),
+	('SAN JOSÉ' , 'SAN JOSÉ'),
+	('EL PLACER' , 'EL PLACER') ,
+
 
 )
 SUB_CUENCAS = (
-	('key' , 'value'),
-	('aa' , 'bb'),
+	('KEY' , 'VALUE'),
+	('AA' , 'BB'),
 
 )
 
 CUENCAS = (
 
-	('KEY' , 'value'),
+	('KEY' , 'VALUE'),
 )
 
 PRESION = (
 
-	('1' , 'val1'),
-	('2' , 'val2'),
-	('3' , 'val3'),
-	('4' , 'val4'),
-	('5' , 'val5'),
+	('1' , 'VAL1'),
+	('2' , 'VAL2'),
+	('3' , 'VAL3'),
+	('4' , 'VAL4'),
+	('5' , 'VAL5'),
 
 )
 
 COLOR = (
-	('key' , 'value'),
+	('KEY' , 'VALUE'),
 )
 
 CABECERA = (
-	('key' , 'value'),
+	('KEY' , 'VALUE'),
 )
 
 TOPO_ESPECIFICACION = (
-	('key' , 'value'),
+	('KEY' , 'VALUE'),
 ) 
 	
 #################################################
