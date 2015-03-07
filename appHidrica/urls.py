d6b23b98abab64803beac888d8c540add0495f07
Arce <arcegk@gmail.com>
2015-03-07 16:26:20 -0500

    second fase

--- appHidrica/urls.py
+++ appHidrica/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from map.views import MapView
+from map.views import MapView , PrintView
 from django.conf import settings
 
 urlpatterns = patterns('',
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
    	url(r'^$', MapView.as_view() , name = "home"),
+   	url(r'^print/$', PrintView.as_view() , name = "print"),
    	url(r'^media/(?P<path>.*)$','django.views.static.serve',
 		{'document_root':settings.MEDIA_ROOT,}
 	),
